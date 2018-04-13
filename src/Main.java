4dd8cb2b83ab874a383d9169a9e685e353e9a1d1
Flavio501 <flavioazul_08@hotmail.com>
2018-04-12 19:36:10 -0700

    API V1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
+import java.net.*;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("TEST 2 alv");
+	public static void main(String[] args) throws Exception {
 
-	}
+        URL oracle = new URL("http://webrates.truefx.com/rates/connect.html?f=html");
+        BufferedReader in = new BufferedReader(
+        new InputStreamReader(oracle.openStream()));
+
+        Document doc;
+        String inputLine, html;
+        while ((inputLine = in.readLine()) != null) {
+        	System.out.println(inputLine);
+        	//System.out.println(Jsoup.parse(inputLine).body().text());
+        }
+        in.close();
+    }
 
 }
